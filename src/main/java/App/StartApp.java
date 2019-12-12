86e8bfe0b4ffe89d1966d1d33fa6202fa7edf379
DawidGrzejek <42537540+DawidGrzejek@users.noreply.github.com>
2019-12-12 16:55:43 +0100

    Update

---
+++ src/main/java/App/StartApp.java
@@ -0,0 +1,11 @@
+package App;
+
+import javafx.application.Application;
+
+import static javafx.application.Application.launch;
+
+public class StartApp {
+    public static void main(String[] args) {
+        Application.launch(Main.class, args);
+    }
+}
---
+++ src/main/java/App/samples.java
@@ -0,0 +1,10 @@
+package App;
+
+public class samples {
+
+
+    public static void main(String[] args) {
+        DBConnect connect = new DBConnect();
+    }
+    
+}
